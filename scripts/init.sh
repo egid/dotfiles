aa699ad463fd78164e973593139debee8a004ce0
David Ganey <david.ganey@redfin.com>
2019-10-18 09:48:40 -0700

    Install oh-my-zsh in init

--- scripts/init.sh
+++ scripts/init.sh
@@ -68,7 +68,8 @@ brew bundle --global
 success 'brew apps installed'
 
 ## Install oh-my-zsh
-# TODO
+status 'Installing oh-my-zsh'
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 ## Install vundle
 status 'checking out vundle (vim bundler)'
